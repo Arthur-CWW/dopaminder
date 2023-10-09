33dd535d91fc47eab0b2a24c0f43bfef23221ed4
chun1 <arthur@chunai.dev>
2023-10-09 22:29:42 +1100

    update names

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,4 @@
-import { exampleRouter } from "~/server/api/routers/example";
+import { sitelistRouter } from "~/server/api/routers/Websitelists";
 import { createTRPCRouter } from "~/server/api/trpc";
 
 /**
@@ -7,7 +7,7 @@ import { createTRPCRouter } from "~/server/api/trpc";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
+  website: sitelistRouter,
 });
 
 // export type definition of API
